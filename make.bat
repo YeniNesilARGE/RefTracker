a1f0a91c622156b462e6fc6ecaec8d498a73408c
Unknown <furkan.sarihan@stu.fsm.edu.tr>
2018-05-01 17:43:33 +0300

    mf filename edited

--- make.bat
+++ make.bat
@@ -1,3 +1,3 @@
 javac src\loginpage\*.java
-jar cvfm RefTracker.jar src\X.MF src\loginpage\*.class
+jar cvfm RefTracker.jar src\RefTracker.MF src\loginpage\*.class
 jar uf RefTracker.jar src\loginpage\*.fxml
