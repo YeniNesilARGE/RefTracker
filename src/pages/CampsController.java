12f4e661a38ebec5057089a23a6c503f3a52ecb2
Bedirhan Yıldırım <bedirhan.yildirim@stu.fsm.edu.tr>
2018-05-06 20:43:43 +0300

    CampPage added

---
+++ src/pages/CampsController.java
@@ -0,0 +1,35 @@
+package src.loginpage;
+
+import javafx.fxml.FXML;
+import javafx.scene.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+
+public class LoginController{
+   @FXML
+   private TableView campsTable;
+   @FXML
+   private Button addCampButton;
+   @FXML
+   private Button findCampButton;
+   @FXML
+   private Button campDetailButton;
+   
+   @FXML
+   private void addClicked(MouseEvent event){
+      //add camp
+   }
+
+   @FXML
+   private void findClicked(MouseEvent event){
+      //find camp
+   }
+
+   @FXML
+   private void detailClicked(MouseEvent event){
+      //camp detail
+   }
+
+}
